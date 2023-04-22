8f5e46d2ecaafcc272838ea83a6693d4d65b1c08
Julius Schmidt <newceptiondev@googlemail.com>
2023-04-22 22:19:04 +0200

    formatting

--- src/app/services/search.service.ts
+++ src/app/services/search.service.ts
@@ -1,29 +1,38 @@
-import { EventEmitter, Injectable } from "@angular/core";
-import { Recipe } from "../models/recipe";
-import { RecipeService } from "./recipe.service";
-import { SearchOptions } from "../models/searchOptions";
+import { EventEmitter, Injectable } from "@angular/core"
+import { Recipe } from "../models/recipe"
+import { RecipeService } from "./recipe.service"
+import { SearchOptions } from "../models/searchOptions"
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: "root",
 })
 export class SearchService {
-
   private publishSearchResults: EventEmitter<Recipe[]> = new EventEmitter<Recipe[]>()
 
-  constructor(private recipeService: RecipeService) { }
+  constructor(private recipeService: RecipeService) {}
 
   public getSearchResultsEventEmitter(): EventEmitter<Recipe[]> {
-    return this.publishSearchResults;
+    return this.publishSearchResults
   }
 
   public search(searchOptions: SearchOptions): void {
     const allRecipes = this.recipeService.getAllRecipes()
 
     const resultList = allRecipes
-      .filter(recipe => recipe.rating >= searchOptions.minimumRating)
-      .filter(recipe => searchOptions.includedCookbooks.length > 0 ? searchOptions.includedCookbooks.includes(recipe.cookbook) : true)
-      .filter(recipe => searchOptions.includedCategories.every(category => recipe.categories.includes(category)))
-      .filter(recipe => searchOptions.requiredIngredients.every(ingredient => recipe.ingredients.includes(ingredient)))
+      .filter((recipe) => recipe.rating >= searchOptions.minimumRating)
+      .filter((recipe) =>
+        searchOptions.includedCookbooks.length > 0
+          ? searchOptions.includedCookbooks.includes(recipe.cookbook)
+          : true
+      )
+      .filter((recipe) =>
+        searchOptions.includedCategories.every((category) => recipe.categories.includes(category))
+      )
+      .filter((recipe) =>
+        searchOptions.requiredIngredients.every((ingredient) =>
+          recipe.ingredients.includes(ingredient)
+        )
+      )
 
     this.publishSearchResults.emit(resultList)
   }
