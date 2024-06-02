73d62359e5807d88f69116162f676216defa943a
Julius Schmidt <newceptiondev@googlemail.com>
2024-06-02 18:42:34 +0200

    angular material mdc migration button

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,7 +4,7 @@ import { BrowserModule } from "@angular/platform-browser"
 import { AppComponent } from "./app.component"
 import { BrowserAnimationsModule } from "@angular/platform-browser/animations"
 import { MatIconModule } from "@angular/material/icon"
-import { MatLegacyButtonModule as MatButtonModule } from "@angular/material/legacy-button"
+import { MatButtonModule } from "@angular/material/button"
 import { MatLegacyInputModule as MatInputModule } from "@angular/material/legacy-input"
 
 import { SidebarComponent } from "./components/sidebar/sidebar.component"
